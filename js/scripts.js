d5d86722107891529a5006b643d524476f81d4eb
sbnawale11 <sbnawale11@gmail.com>
2024-12-20 15:53:59 -0800

    Errors removed

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log("Hi! Welcome to my Portfolio site")
+console.log("Hi! Welcome to my Portfolio site");
+
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
   }
+}
